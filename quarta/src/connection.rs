af560cdca88ff6f4756034dd758033370a98d987
Andy Grover <agrover@mozilla.com>
2019-02-19 20:52:14 -0800

    fix TODOs as per ekr
    and some whitespace

--- quarta/src/connection.rs
+++ quarta/src/connection.rs
@@ -1,20 +1,19 @@
 use std::collections::{HashMap, HashSet};
-use std::net::{SocketAddr};
+use std::net::SocketAddr;
 
 use crate::data::Data;
 use crate::frame::{decode_frame, Frame};
 use crate::stream::Stream;
 use crate::{Error, Res};
-//use qinterface::{QInterface, StreamStateQuery, StateInfo, StreamInfo, ConnState, InterfaceError};
+//use qinterface::{ConnState, InterfaceError, QInterface, StateInfo, StreamInfo, StreamStateQuery};
 
 #[derive(Debug, Default)]
 struct Packet(Vec<u8>);
 
-
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Role {
     Client,
-    Server
+    Server,
 }
 
 #[derive(Debug, PartialEq)]
@@ -46,9 +45,12 @@ pub struct Connection {
 
 impl Connection {
     pub fn new(r: Role) -> Connection {
-        Connection{
+        Connection {
             role: r,
-            state: match r { Role::Client => State::Init, Role::Server => State::WaitInitial },
+            state: match r {
+                Role::Client => State::Init,
+                Role::Server => State::WaitInitial,
+            },
             deadline: 0,
             max_data: 0,
             max_streams: 0,
@@ -56,18 +58,20 @@ impl Connection {
             connection_ids: HashSet::new(),
             next_stream_id: 0,
             streams: HashMap::new(),
-            outgoing_pkts: Vec::new()
+            outgoing_pkts: Vec::new(),
         }
     }
 
     pub fn input(&mut self, _d: &Datagram, now: u64) -> Res<(&Datagram, u64)> {
         // TODO(ekr@rtfm.com): Process the incoming packets.
-        
+
         if now > self.deadline {
             // Timer expired.
             match self.state {
-                State::Init => { self.client_start()?;}
-                _ => unimplemented!()
+                State::Init => {
+                    self.client_start()?;
+                }
+                _ => unimplemented!(),
             }
         }
 
@@ -75,9 +79,9 @@ impl Connection {
     }
 
     fn client_start(&mut self) -> Res<(&Datagram, u64)> {
-        Err(Error::ErrInternal)        
+        Err(Error::ErrInternal)
     }
-    
+
     pub fn process_input_frame(&mut self, frame: &[u8]) -> Res<()> {
         let mut data = Data::from_slice(frame);
         let frame = decode_frame(&mut data)?;
@@ -87,23 +91,23 @@ impl Connection {
             Frame::Padding => {
                 println!("padding!");
             }
-            Frame::Ping => {} // TODO generate ack
+            Frame::Ping => {} // TODO(agrover@mozilla.com): generate ack
             Frame::Ack {
                 largest_acknowledged,
                 ack_delay,
                 first_ack_range,
                 ack_ranges,
-            } => {} // TODO remove acked ranges from list of in-flight packets
+            } => {} // TODO(agrover@mozilla.com): remove acked ranges from list of in-flight packets
             Frame::ResetStream {
                 stream_id,
                 application_error_code,
                 final_size,
-            } => {} // TODO reset a stream
+            } => {} // TODO(agrover@mozilla.com): reset a stream
             Frame::StopSending {
                 application_error_code,
-            } => {} // TODO stop sending on a stream
-            Frame::Crypto { offset, data } => {} // TODO pass to crypto handling code
-            Frame::NewToken { token } => {} // TODO stick the new token somewhere
+            } => {} // TODO(agrover@mozilla.com): stop sending on a stream
+            Frame::Crypto { offset, data } => {} // TODO(agrover@mozilla.com): pass to crypto handling code
+            Frame::NewToken { token } => {} // TODO(agrover@mozilla.com): stick the new token somewhere
             Frame::Stream {
                 fin,
                 stream_id,
@@ -112,24 +116,24 @@ impl Connection {
             } => {
                 self.process_inbound_stream_frame(fin, stream_id, offset, data)?;
             }
-            Frame::MaxData { maximum_data } => {} // TODO set self.max_data?
+            Frame::MaxData { maximum_data } => {} // TODO(agrover@mozilla.com): set self.max_data?
             Frame::MaxStreamData {
                 stream_id,
                 maximum_stream_data,
-            } => {} // TODO lookup stream and modify its max_stream_data
+            } => {} // TODO(agrover@mozilla.com): lookup stream and modify its max_stream_data
             Frame::MaxStreams {
                 stream_type,
                 maximum_streams,
-            } => {} // TODO adjust self.max_streams?
-            Frame::DataBlocked { data_limit } => {} // TODO use as input to flow control algorithms
+            } => {} // TODO(agrover@mozilla.com): adjust self.max_streams?
+            Frame::DataBlocked { data_limit } => {} // TODO(agrover@mozilla.com): use as input to flow control algorithms
             Frame::StreamDataBlocked {
                 stream_id,
                 stream_data_limit,
-            } => {} // TODO do something
+            } => {} // TODO(agrover@mozilla.com): do something
             Frame::StreamsBlocked {
                 stream_type,
                 stream_limit,
-            } => {} // TODO do something
+            } => {} // TODO(agrover@mozilla.com): do something
             Frame::NewConnectionId {
                 sequence_number,
                 connection_id,
@@ -137,15 +141,15 @@ impl Connection {
             } => {
                 self.connection_ids.insert((sequence_number, connection_id));
             }
-            Frame::RetireConnectionId { sequence_number } => {} // TODO remove from list of connection IDs
-            Frame::PathChallenge { data } => {}                 // TODO generate PATH_RESPONSE
-            Frame::PathResponse { data } => {}                  // TODO do something
+            Frame::RetireConnectionId { sequence_number } => {} // TODO(agrover@mozilla.com): remove from list of connection IDs
+            Frame::PathChallenge { data } => {} // TODO(agrover@mozilla.com): generate PATH_RESPONSE
+            Frame::PathResponse { data } => {}  // TODO(agrover@mozilla.com): do something
             Frame::ConnectionClose {
                 close_type,
                 error_code,
                 frame_type,
                 reason_phrase,
-            } => {} // TODO close the connection
+            } => {} // TODO(agrover@mozilla.com): close the connection
         };
 
         Ok(())
@@ -158,7 +162,7 @@ impl Connection {
         offset: u64,
         data: Vec<u8>,
     ) -> Res<()> {
-        // TODO check against list of ooo frames and maybe make some data available
+        // TODO(agrover@mozilla.com): check against list of ooo frames and maybe make some data available
         let stream = self
             .streams
             .get_mut(&stream_id)
@@ -167,9 +171,9 @@ impl Connection {
         //let end_offset = offset + data.len() as u64;
         if offset == stream.next_rx_offset() {
             // in order!
-            // TODO make data available to upper layers
+            // TODO(agrover@mozilla.com): make data available to upper layers
             stream.data_ready(&data);
-            // TODO generate ACK frames
+            // TODO(agrover@mozilla.com): generate ACK frames
         }
         if fin {
             println!("fin set!")
@@ -189,12 +193,18 @@ impl Connection {
 
 /*
 impl QInterface<Stream> for Connection {
-  fn incoming_datagram(&mut self, path: SocketAddr, frame: &[u8], time: u64, streamStateQuery: StreamStateQuery) -> std::result::Result<StateInfo, InterfaceError> {
-    Err(InterfaceError::NOT_IMPLEMENTED)
-  }
+    fn incoming_datagram(
+        &mut self,
+        path: SocketAddr,
+        frame: &[u8],
+        time: u64,
+        streamStateQuery: StreamStateQuery,
+    ) -> std::result::Result<StateInfo, InterfaceError> {
+        Err(InterfaceError::NOT_IMPLEMENTED)
+    }
 
-  fn GetStream (&self, streamId: u64) -> Option<&Stream> {
-     self.streams.get(&streamId)
-  }
+    fn GetStream(&self, streamId: u64) -> Option<&Stream> {
+        self.streams.get(&streamId)
+    }
 }
 */
