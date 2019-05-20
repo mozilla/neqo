680bce440f08b4be797209b521294a7ab0a7e9d7
Andy Grover <agrover@mozilla.com>
2019-05-20 08:12:20 -0700

    update http3-client to also support 0.9
    Severe duplication, since the implementations are not easily
    abstractable.

--- neqo-http3-client/src/main.rs
+++ neqo-http3-client/src/main.rs
@@ -55,7 +55,7 @@ impl FromStr for Headers {
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "neqo-http3-client", about = "A basic HTTP3 client.")]
-struct Args {
+pub struct Args {
     #[structopt(short = "d", long, default_value = "./db", parse(from_os_str))]
     db: PathBuf,
 
@@ -78,6 +78,10 @@ struct Args {
 
     #[structopt(short = "b", long, default_value = "128")]
     max_blocked_streams: u16,
+
+    #[structopt(short = "o", long)]
+    /// Use http 0.9 instead of HTTP/3
+    use_old_http: bool,
 }
 
 impl Args {
@@ -228,30 +232,7 @@ impl Handler for PostConnectHandler {
     }
 }
 
-fn main() {
-    let args = Args::from_args();
-    init_db(args.db.clone());
-
-    let remote_addr = match args.remote_addr() {
-        Err(e) => {
-            eprintln!("Unable to resolve remote addr: {}", e);
-            exit(1)
-        }
-        Ok(addr) => addr,
-    };
-    let socket = match args.local_addr().and_then(|args| UdpSocket::bind(args)) {
-        Err(e) => {
-            eprintln!("Unable to bind UDP socket: {}", e);
-            exit(1)
-        }
-        Ok(s) => s,
-    };
-    socket.connect(&args).expect("Unable to connect UDP socket");
-
-    let local_addr = socket.local_addr().expect("Socket local address not bound");
-
-    println!("Client connecting: {:?} -> {:?}", local_addr, remote_addr);
-
+fn client(args: Args, socket: UdpSocket, local_addr: SocketAddr, remote_addr: SocketAddr) {
     let mut client = Http3Connection::new(
         Connection::new_client(
             args.url.host_str().unwrap(),
@@ -280,4 +261,183 @@ fn main() {
     let mut h2 = PostConnectHandler::default();
     h2.streams.insert(client_stream_id);
     process_loop(&local_addr, &remote_addr, &socket, &mut client, &mut h2);
+}
+
+fn main() {
+    let args = Args::from_args();
+    init_db(args.db.clone());
+
+    let remote_addr = match args.remote_addr() {
+        Err(e) => {
+            eprintln!("Unable to resolve remote addr: {}", e);
+            exit(1)
+        }
+        Ok(addr) => addr,
+    };
+    let socket = match args.local_addr().and_then(|args| UdpSocket::bind(args)) {
+        Err(e) => {
+            eprintln!("Unable to bind UDP socket: {}", e);
+            exit(1)
+        }
+        Ok(s) => s,
+    };
+    socket.connect(&args).expect("Unable to connect UDP socket");
+
+    let local_addr = socket.local_addr().expect("Socket local address not bound");
+
+    println!("Client connecting: {:?} -> {:?}", local_addr, remote_addr);
+
+    if args.use_old_http {
+        old::old_client(args, socket, local_addr, remote_addr)
+    } else {
+        client(args, socket, local_addr, remote_addr)
+    }
+}
+
+mod old {
+    use std::collections::HashSet;
+    use std::net::{SocketAddr, UdpSocket};
+    use std::process::exit;
+
+    use neqo_common::now;
+    use neqo_transport::frame::StreamType;
+    use neqo_transport::{Connection, ConnectionEvent, Datagram, State};
+
+    use super::{emit_packets, Args};
+
+    trait HandlerOld {
+        fn handle(&mut self, client: &mut Connection) -> bool;
+    }
+
+    struct PreConnectHandlerOld {}
+    impl HandlerOld for PreConnectHandlerOld {
+        fn handle(&mut self, client: &mut Connection) -> bool {
+            if let State::Connected = dbg!(client.state()) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    #[derive(Default)]
+    struct PostConnectHandlerOld {
+        streams: HashSet<u64>,
+    }
+
+    // This is a bit fancier than actually needed.
+    impl HandlerOld for PostConnectHandlerOld {
+        fn handle(&mut self, client: &mut Connection) -> bool {
+            let mut data = vec![0; 4000];
+            for event in client.events() {
+                match event {
+                    ConnectionEvent::RecvStreamReadable { stream_id } => {
+                        if !self.streams.contains(&stream_id) {
+                            println!("Data on unexpected stream: {}", stream_id);
+                            return false;
+                        }
+
+                        let (_sz, fin) = client
+                            .stream_recv(stream_id, &mut data)
+                            .expect("Read should succeed");
+                        println!(
+                            "READ[{}]: {}",
+                            stream_id,
+                            String::from_utf8(data.clone()).unwrap()
+                        );
+                        if fin {
+                            println!("<FIN[{}]>", stream_id);
+                            client.close(0, "kthxbye!");
+                            return false;
+                        }
+                    }
+                    ConnectionEvent::SendStreamWritable { stream_id } => {
+                        println!("stream {} writable", stream_id)
+                    }
+                    _ => {
+                        println!("Unexpected event {:?}", event);
+                    }
+                }
+            }
+
+            true
+        }
+    }
+
+    fn process_loop_old(
+        local_addr: &SocketAddr,
+        remote_addr: &SocketAddr,
+        socket: &UdpSocket,
+        client: &mut Connection,
+        handler: &mut HandlerOld,
+    ) -> neqo_transport::connection::State {
+        let buf = &mut [0u8; 2048];
+        let mut in_dgrams = Vec::new();
+        loop {
+            client.process_input(in_dgrams.drain(..), now());
+
+            if let State::Closed(..) = client.state() {
+                return client.state().clone();
+            }
+
+            let exiting = !handler.handle(client);
+
+            let (out_dgrams, _timer) = client.process_output(now());
+            emit_packets(&socket, &out_dgrams);
+
+            if exiting {
+                return client.state().clone();
+            }
+
+            let sz = match socket.recv(&mut buf[..]) {
+                Err(err) => {
+                    eprintln!("UDP error: {}", err);
+                    exit(1)
+                }
+                Ok(sz) => sz,
+            };
+            if sz == buf.len() {
+                eprintln!("Received more than {} bytes", buf.len());
+                continue;
+            }
+            if sz > 0 {
+                in_dgrams.push(Datagram::new(
+                    remote_addr.clone(),
+                    local_addr.clone(),
+                    &buf[..sz],
+                ));
+            }
+        }
+    }
+
+    pub fn old_client(
+        args: Args,
+        socket: UdpSocket,
+        local_addr: SocketAddr,
+        remote_addr: SocketAddr,
+    ) {
+        dbg!(args.url.host_str().unwrap());
+        dbg!(&args.alpn);
+        dbg!(local_addr);
+        dbg!(remote_addr);
+
+        let mut client = Connection::new_client(
+            args.url.host_str().unwrap(),
+            vec!["http/0.9"],
+            local_addr,
+            remote_addr,
+        )
+        .expect("must succeed");
+        // Temporary here to help out the type inference engine
+        let mut h = PreConnectHandlerOld {};
+        process_loop_old(&local_addr, &remote_addr, &socket, &mut client, &mut h);
+
+        let client_stream_id = client.stream_create(StreamType::BiDi).unwrap();
+        let req: String = "GET /10\r\n".to_string();
+        client
+            .stream_send(client_stream_id, req.as_bytes())
+            .unwrap();
+        let mut h2 = PostConnectHandlerOld::default();
+        h2.streams.insert(client_stream_id);
+        process_loop_old(&local_addr, &remote_addr, &socket, &mut client, &mut h2);
+    }
 }
