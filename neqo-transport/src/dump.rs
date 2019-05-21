5a3ca205e27cb3724e990dd9ab0c2a0cbf803adf
EKR <ekr@rtfm.com>
2019-05-21 10:08:22 -0700

    Added

---
+++ neqo-transport/src/dump.rs
@@ -0,0 +1,29 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![allow(dead_code)]
+use crate::connection::Connection;
+use crate::frame::decode_frame;
+use crate::packet::PacketHdr;
+use neqo_common::{qdebug, Decoder};
+pub fn dump_packet(conn: &Connection, dir: &str, hdr: &PacketHdr, payload: &[u8]) {
+    let mut s = String::from("");
+    let mut d = Decoder::from(payload);
+    while d.remaining() > 0 {
+        let f = match decode_frame(&mut d) {
+            Ok(f) => f,
+            Err(_) => {
+                s.push_str(" [broken]...");
+                break;
+            }
+        };
+        if let Some(x) = f.dump() {
+            s.push_str(" ");
+            s.push_str(&x);
+        }
+    }
+    qdebug!([conn] "{}: type={:?} pn={}: {}", dir, hdr.tipe, hdr.pn, s);
+}
