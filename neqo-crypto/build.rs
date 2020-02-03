61001627cd2a83c67b146db2f80156497c5c679c
Andy Grover <agrover@mozilla.com>
2020-02-03 15:09:33 +0100

    Revert move to bindgen 52
    Unfortunately Gecko is not cool with bindgen 52.

--- neqo-crypto/build.rs
+++ neqo-crypto/build.rs
@@ -226,6 +226,7 @@ fn build_bindings(base: &str, bindings: &Bindings, flags: &[String], gecko: bool
 
     let mut builder = Builder::default().header(header);
     builder = builder.generate_comments(false);
+    builder = builder.derive_debug(false); // https://github.com/rust-lang/rust-bindgen/issues/372
 
     builder = builder.clang_arg("-v");
 
