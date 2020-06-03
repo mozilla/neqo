9dee8510ee3fc2e2423092f095cf2dfee34c1063
Andy Grover <agrover@mozilla.com>
2020-06-03 10:25:37 -0700

    Add support for new draft 28 tparams
    ACTIVE_CONNECTION_ID_LIMIT integer
    INITIAL_SOURCE_CONNECTION_ID bytes
    RETRY_SOURCE_CONNECTION_ID bytes

--- neqo-transport/src/tparams.rs
+++ neqo-transport/src/tparams.rs
@@ -27,20 +27,23 @@ macro_rules! tpids {
         };
     }
 tpids! {
-    ORIGINAL_CONNECTION_ID = 0,
-    IDLE_TIMEOUT = 1,
-    STATELESS_RESET_TOKEN = 2,
-    MAX_UDP_PAYLOAD_SIZE = 3,
-    INITIAL_MAX_DATA = 4,
-    INITIAL_MAX_STREAM_DATA_BIDI_LOCAL = 5,
-    INITIAL_MAX_STREAM_DATA_BIDI_REMOTE = 6,
-    INITIAL_MAX_STREAM_DATA_UNI = 7,
-    INITIAL_MAX_STREAMS_BIDI = 8,
-    INITIAL_MAX_STREAMS_UNI = 9,
-    ACK_DELAY_EXPONENT = 10,
-    MAX_ACK_DELAY = 11,
-    DISABLE_MIGRATION = 12,
-    PREFERRED_ADDRESS = 13,
+    ORIGINAL_CONNECTION_ID = 0x00,
+    IDLE_TIMEOUT = 0x01,
+    STATELESS_RESET_TOKEN = 0x02,
+    MAX_UDP_PAYLOAD_SIZE = 0x03,
+    INITIAL_MAX_DATA = 0x04,
+    INITIAL_MAX_STREAM_DATA_BIDI_LOCAL = 0x05,
+    INITIAL_MAX_STREAM_DATA_BIDI_REMOTE = 0x06,
+    INITIAL_MAX_STREAM_DATA_UNI = 0x07,
+    INITIAL_MAX_STREAMS_BIDI = 0x08,
+    INITIAL_MAX_STREAMS_UNI = 0x09,
+    ACK_DELAY_EXPONENT = 0x0a,
+    MAX_ACK_DELAY = 0x0b,
+    DISABLE_MIGRATION = 0x0c,
+    PREFERRED_ADDRESS = 0x0d,
+    ACTIVE_CONNECTION_ID_LIMIT = 0x0e,
+    INITIAL_SOURCE_CONNECTION_ID = 0x0f,
+    RETRY_SOURCE_CONNECTION_ID = 0x10,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -81,7 +84,9 @@ impl TransportParameter {
         qtrace!("TP {:x} length {:x}", tp, content.len());
         let mut d = Decoder::from(content);
         let value = match tp {
-            ORIGINAL_CONNECTION_ID => Self::Bytes(d.decode_remainder().to_vec()),
+            ORIGINAL_CONNECTION_ID | INITIAL_SOURCE_CONNECTION_ID | RETRY_SOURCE_CONNECTION_ID => {
+                Self::Bytes(d.decode_remainder().to_vec())
+            }
             STATELESS_RESET_TOKEN => {
                 if d.remaining() != 16 {
                     return Err(Error::TransportParameterError);
@@ -110,6 +115,10 @@ impl TransportParameter {
 
             ACK_DELAY_EXPONENT => match d.decode_varint() {
                 Some(v) if v <= 20 => Self::Integer(v),
+                _ => return Err(Error::TransportParameterError),
+            },
+            ACTIVE_CONNECTION_ID_LIMIT => match d.decode_varint() {
+                Some(v) if v <= 2 => Self::Integer(v),
                 _ => return Err(Error::TransportParameterError),
             },
 
@@ -178,6 +187,7 @@ impl TransportParameters {
             MAX_UDP_PAYLOAD_SIZE => 65527,
             ACK_DELAY_EXPONENT => 3,
             MAX_ACK_DELAY => 25,
+            ACTIVE_CONNECTION_ID_LIMIT => 2,
             _ => panic!("Transport parameter not known or not an Integer"),
         };
         match self.params.get(&tp) {
@@ -187,7 +197,7 @@ impl TransportParameters {
         }
     }
 
-    // Get an integer type or a default.
+    // Set an integer type or a default.
     pub fn set_integer(&mut self, tp: TransportParameterId, value: u64) {
         match tp {
             IDLE_TIMEOUT
@@ -199,7 +209,8 @@ impl TransportParameters {
             | INITIAL_MAX_STREAMS_UNI
             | MAX_UDP_PAYLOAD_SIZE
             | ACK_DELAY_EXPONENT
-            | MAX_ACK_DELAY => {
+            | MAX_ACK_DELAY
+            | ACTIVE_CONNECTION_ID_LIMIT => {
                 self.set(tp, TransportParameter::Integer(value));
             }
             _ => panic!("Transport parameter not known"),
@@ -208,7 +219,10 @@ impl TransportParameters {
 
     pub fn get_bytes(&self, tp: TransportParameterId) -> Option<&[u8]> {
         match tp {
-            ORIGINAL_CONNECTION_ID | STATELESS_RESET_TOKEN => {}
+            ORIGINAL_CONNECTION_ID
+            | INITIAL_SOURCE_CONNECTION_ID
+            | RETRY_SOURCE_CONNECTION_ID
+            | STATELESS_RESET_TOKEN => {}
             _ => panic!("Transport parameter not known or not type bytes"),
         }
 
@@ -221,7 +235,10 @@ impl TransportParameters {
 
     pub fn set_bytes(&mut self, tp: TransportParameterId, value: Vec<u8>) {
         match tp {
-            ORIGINAL_CONNECTION_ID | STATELESS_RESET_TOKEN => {
+            ORIGINAL_CONNECTION_ID
+            | INITIAL_SOURCE_CONNECTION_ID
+            | RETRY_SOURCE_CONNECTION_ID
+            | STATELESS_RESET_TOKEN => {
                 self.set(tp, TransportParameter::Bytes(value));
             }
             _ => panic!("Transport parameter not known or not type bytes"),
@@ -258,10 +275,13 @@ impl TransportParameters {
             if matches!(
                 *k,
                 ORIGINAL_CONNECTION_ID
+                    | INITIAL_SOURCE_CONNECTION_ID
+                    | RETRY_SOURCE_CONNECTION_ID
                     | STATELESS_RESET_TOKEN
                     | IDLE_TIMEOUT
                     | ACK_DELAY_EXPONENT
                     | MAX_ACK_DELAY
+                    | ACTIVE_CONNECTION_ID_LIMIT
             ) {
                 continue;
             }
@@ -408,10 +428,15 @@ mod tests {
         println!("TPS = {:?}", tps);
         assert_eq!(tps2.get_integer(IDLE_TIMEOUT), 0); // Default
         assert_eq!(tps2.get_integer(MAX_ACK_DELAY), 25); // Default
+        assert_eq!(tps2.get_integer(ACTIVE_CONNECTION_ID_LIMIT), 2); // Default
         assert_eq!(tps2.get_integer(INITIAL_MAX_STREAMS_BIDI), 10); // Sent
         assert_eq!(tps2.get_bytes(STATELESS_RESET_TOKEN), Some(RESET_TOKEN));
         assert_eq!(tps2.get_bytes(ORIGINAL_CONNECTION_ID), None);
+        assert_eq!(tps2.get_bytes(INITIAL_SOURCE_CONNECTION_ID), None);
+        assert_eq!(tps2.get_bytes(RETRY_SOURCE_CONNECTION_ID), None);
         assert_eq!(tps2.was_sent(ORIGINAL_CONNECTION_ID), false);
+        assert_eq!(tps2.was_sent(INITIAL_SOURCE_CONNECTION_ID), false);
+        assert_eq!(tps2.was_sent(RETRY_SOURCE_CONNECTION_ID), false);
         assert_eq!(tps2.was_sent(STATELESS_RESET_TOKEN), true);
 
         let mut enc = Encoder::default();
@@ -429,6 +454,7 @@ mod tests {
         );
         tps_a.set(IDLE_TIMEOUT, TransportParameter::Integer(10));
         tps_a.set(MAX_ACK_DELAY, TransportParameter::Integer(22));
+        tps_a.set(ACTIVE_CONNECTION_ID_LIMIT, TransportParameter::Integer(33));
 
         let mut tps_b = TransportParameters::default();
         assert!(tps_a.ok_for_0rtt(&tps_b));
@@ -440,6 +466,7 @@ mod tests {
         );
         tps_b.set(IDLE_TIMEOUT, TransportParameter::Integer(100));
         tps_b.set(MAX_ACK_DELAY, TransportParameter::Integer(2));
+        tps_b.set(ACTIVE_CONNECTION_ID_LIMIT, TransportParameter::Integer(44));
         assert!(tps_a.ok_for_0rtt(&tps_b));
         assert!(tps_b.ok_for_0rtt(&tps_a));
     }
