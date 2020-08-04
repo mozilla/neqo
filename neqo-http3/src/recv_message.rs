640f7924fcc2f0139f9b4fc5f75f628d76686f0e
Dragana Damjanovic <dragana.damjano@gmail.com>
2020-08-04 22:04:07 +0200

    Address comments

--- neqo-http3/src/recv_message.rs
+++ neqo-http3/src/recv_message.rs
@@ -294,6 +294,13 @@ impl RecvMessage {
         }
         self.state = RecvMessageState::Closed;
     }
+
+    fn closing(&self) -> bool {
+        matches!(
+            self.state,
+            RecvMessageState::ClosePending | RecvMessageState::Closed
+        )
+    }
 }
 
 impl RecvStream for RecvMessage {
@@ -324,22 +331,14 @@ impl RecvStream for RecvMessage {
     }
 
     fn stream_reset_recv(&self, app_error: AppError, decoder: &mut QPackDecoder) {
-        if !matches!(
-            self.state,
-            RecvMessageState::ClosePending | RecvMessageState::Closed
-        ) || !self.blocked_push_promise.is_empty()
-        {
+        if !self.closing() || !self.blocked_push_promise.is_empty() {
             decoder.cancel_stream(self.stream_id);
         }
         self.conn_events.reset(self.stream_id, app_error);
     }
 
     fn stream_reset(&self, decoder: &mut QPackDecoder) {
-        if !matches!(
-            self.state,
-            RecvMessageState::ClosePending | RecvMessageState::Closed
-        ) || !self.blocked_push_promise.is_empty()
-        {
+        if !self.closing() || !self.blocked_push_promise.is_empty() {
             decoder.cancel_stream(self.stream_id);
         }
     }
