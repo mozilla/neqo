6deb08b442333fb3da35bc8575b83ba7d1da7c7e
Andy Grover <agrover@mozilla.com>
2020-08-09 13:30:35 -0700

    QNS client: Re-add sleep to client runs
    We're seeing "connection refused" errors. wait-for-it doesn't seem to ever
    be waiting.
    
    We need to re-add the sleep for now.

--- neqo-qns/run_endpoint.sh
+++ neqo-qns/run_endpoint.sh
@@ -13,6 +13,7 @@ if [ "$ROLE" == "client" ]; then
     echo "Starting Neqo client ..."
     echo "CLIENT_PARAMS: ${CLIENT_PARAMS[*]}"
     echo "REQUESTS: $REQUESTS"
+    sleep 5
     RUST_LOG=debug RUST_BACKTRACE=1 ./target/neqo-client "${CLIENT_PARAMS[@]}" $REQUESTS
 elif [ "$ROLE" == "server" ]; then
     RUST_LOG=info RUST_BACKTRACE=1 ./target/neqo-http3-server "${SERVER_PARAMS[@]}"
