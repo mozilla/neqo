0f21dabfd42ffaf478b1a99a14ca02a853f5576f
Martin Thomson <mt@lowentropy.net>
2020-08-19 16:01:13 +1000

    Make push optional

--- neqo-qns/update.sh
+++ neqo-qns/update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [[ "$1" == "-p" ]]; then
+  shift
+  push=1
+else
+  push=0
+fi
+
 tag="neqoquic/neqo-qns:${1:-latest}"
 branch="${1:-main}"
 
@@ -9,12 +16,16 @@ cd "$(dirname "$0")"
 rev=$(git log -n 1 --format='format:%H' -- .)
 [[ "$rev" != "$(cat .last-update)" ]] || (echo "No change since $rev."; exit 0)
 
+# This makes the local .git directory the source, allowing for the current
+# build to be build and pushed.
 [[ ! -e .git ]] || ! echo "Found .git directory. Script still active. Exiting."
 trap 'rm -rf .git' EXIT
 cp -R ../.git .git
 
 docker build -t "$tag" --build-arg NEQO_BRANCH="$branch" .
-docker login
-docker push "$tag"
+if [[ "$push" == "1" ]]; then
+  docker login
+  docker push "$tag"
+fi
 
 echo "$rev" > .last-update
