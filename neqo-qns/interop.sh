a5529cbb0798e440608cb157b81bb83e10187cb9
Martin Thomson <mt@lowentropy.net>
2020-09-22 08:50:16 +1000

    Use provided certificates in interop server

---
+++ neqo-qns/interop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+/setup.sh
+
+set -ex
+
+export PATH="${PATH}:/neqo/bin"
+
+case "$ROLE" in
+  client)
+    /wait-for-it.sh sim:57832 -s -t 30
+    sleep 5
+    RUST_LOG=debug RUST_BACKTRACE=1 neqo-client --qns-mode --output-dir /downloads $REQUESTS
+    ;;
+
+  server)
+    DB=/neqo/db
+    CERT=cert
+    P12CERT=$(mktemp)
+    mkdir -p "$DB"
+    certutil -N -d "sql:$DB" --empty-password
+    openssl pkcs12 -export -nodes -in /certs/cert.pem -inkey /certs/priv.key \
+        -name "$CERT" -passout pass: -out "$P12CERT"
+    pk12util -d "sql:$DB" -i "$P12CERT" -W ''
+    certutil -L -d "sql:$DB" -n "$CERT"
+    RUST_LOG=info RUST_BACKTRACE=1 neqo-server --qns-mode -d "$DB" -k "$CERT" 0.0.0.0:443
+    ;;
+
+  *)
+    exit 1
+    ;;
+esac
