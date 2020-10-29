b649a4f3fb517c3c8555114dd98e6dcb80e07968
Dragana Damjanovic <dd.mozilla@gmail.com>
2020-10-29 16:34:47 +0100

    Expose HFrameReader, QPackEncoder and QPackDecoder outside of the crate. It is needed for necko tests (#1025)
    Expose HFrameReader, QPackEncoder and QPackDecoder outside of the crate. It is needed for necko tests

--- neqo-http3/src/lib.rs
+++ neqo-http3/src/lib.rs
@@ -36,6 +36,7 @@ pub use client_events::Http3ClientEvent;
 pub use connection::Http3State;
 pub use connection_client::Http3Client;
 pub use connection_client::Http3Parameters;
+pub use hframe::HFrameReader;
 pub use neqo_qpack::Header;
 pub use server::Http3Server;
 pub use server_events::Http3ServerEvent;
