f28977b60485bcaa08dbb65f15b97c99491b5980
Dragana Damjanovic <dragana.damjano@gmail.com>
2021-06-22 12:08:21 +0200

    Address comment

--- neqo-http3/src/connection_server.rs
+++ neqo-http3/src/connection_server.rs
@@ -221,17 +221,12 @@ impl Http3ServerHandler {
             .http_stream()
             .ok_or(Error::InvalidStreamId)?;
 
-        match recv_stream.read_data(conn, buf) {
-            Ok((amount, fin)) => {
-                if recv_stream.done() {
-                    self.base_handler.recv_streams.remove(&stream_id);
-                }
-                Ok((amount, fin))
-            }
-            Err(e) => {
-                self.close(conn, now, &e);
-                Err(e)
-            }
+        let res = recv_stream.read_data(conn, buf);
+        if let Err(e) = &res {
+            self.close(conn, now, e);
+        } else if recv_stream.done() {
+            self.base_handler.recv_streams.remove(&stream_id);
         }
+        res
     }
 }
