2784db9a97fff351896110c9d5c8a3c4bcb7d247
Martin Thomson <mt@lowentropy.net>
2021-09-17 14:51:28 +1000

    Rework frames that fill packets
    I started out just looking at the DATAGRAM frame, for which we need to
    be careful in two ways: we need to ensure that the entire payload is
    sent in one piece, and we need to ensure that when we using the packet
    filling frame type, we don't follow the frame with other frames.
    
    Then I looked at the STREAM frame and what we are doing was wasteful.
    We were adding a length field just to fill the packet, even when we
    could have used the packet filling option.  That was wasteful, because
    the extra length field meant that we sent a larger packet than was
    necessary.
    
    This logic turns out to be a whole lot simpler.  And we save a byte or
    two here and there.
    
    The negative side of this is that those byte savings might make our
    packets ever-so-slightly more vulnerable to traffic analysis.  And the
    way that packets are marked as "full" will make padding them more
    challenging later.  I'm not overly concerned about this as the change is
    tiny and we will need special logic for padding of packet-filling frames
    anyway.
    
    Closes #1229.

--- neqo-transport/src/connection/tests/datagram.rs
+++ neqo-transport/src/connection/tests/datagram.rs
@@ -14,7 +14,9 @@ use crate::packet::PacketBuilder;
 use crate::quic_datagrams::MAX_QUIC_DATAGRAM;
 use crate::{Connection, ConnectionError, ConnectionParameters, Error};
 use neqo_common::event::Provider;
+use std::cell::RefCell;
 use std::convert::TryFrom;
+use std::rc::Rc;
 use test_fixture::now;
 
 const DATAGRAM_LEN_MTU: u64 = 1310;
@@ -345,15 +347,15 @@ fn outgoing_datagram_queue_full() {
     ));
 }
 
-fn send_datagram(client: &mut Connection, server: &mut Connection, data: &[u8]) {
-    let dgram_sent = server.stats().frame_tx.datagram;
-    assert_eq!(server.send_datagram(data, Some(1)), Ok(()));
-    let out = server.process_output(now()).dgram().unwrap();
-    assert_eq!(server.stats().frame_tx.datagram, dgram_sent + 1);
+fn send_datagram(sender: &mut Connection, receiver: &mut Connection, data: &[u8]) {
+    let dgram_sent = sender.stats().frame_tx.datagram;
+    assert_eq!(sender.send_datagram(data, Some(1)), Ok(()));
+    let out = sender.process_output(now()).dgram().unwrap();
+    assert_eq!(sender.stats().frame_tx.datagram, dgram_sent + 1);
 
-    let dgram_received = client.stats().frame_rx.datagram;
-    client.process_input(out, now());
-    assert_eq!(client.stats().frame_rx.datagram, dgram_received + 1);
+    let dgram_received = receiver.stats().frame_rx.datagram;
+    receiver.process_input(out, now());
+    assert_eq!(receiver.stats().frame_rx.datagram, dgram_received + 1);
 }
 
 #[test]
@@ -373,9 +375,9 @@ fn multiple_datagram_events() {
     let mut server = default_server();
     connect_force_idle(&mut client, &mut server);
 
-    send_datagram(&mut client, &mut server, FIRST_DATAGRAM);
-    send_datagram(&mut client, &mut server, SECOND_DATAGRAM);
-    send_datagram(&mut client, &mut server, THIRD_DATAGRAM);
+    send_datagram(&mut server, &mut client, FIRST_DATAGRAM);
+    send_datagram(&mut server, &mut client, SECOND_DATAGRAM);
+    send_datagram(&mut server, &mut client, THIRD_DATAGRAM);
 
     let mut datagrams = client.events().filter_map(|evt| {
         if let ConnectionEvent::Datagram(d) = evt {
@@ -390,7 +392,7 @@ fn multiple_datagram_events() {
     assert!(datagrams.next().is_none());
 
     // New events can be queued.
-    send_datagram(&mut client, &mut server, FOURTH_DATAGRAM);
+    send_datagram(&mut server, &mut client, FOURTH_DATAGRAM);
     let mut datagrams = client.events().filter_map(|evt| {
         if let ConnectionEvent::Datagram(d) = evt {
             Some(d)
@@ -419,9 +421,9 @@ fn too_many_datagram_events() {
     let mut server = default_server();
     connect_force_idle(&mut client, &mut server);
 
-    send_datagram(&mut client, &mut server, FIRST_DATAGRAM);
-    send_datagram(&mut client, &mut server, SECOND_DATAGRAM);
-    send_datagram(&mut client, &mut server, THIRD_DATAGRAM);
+    send_datagram(&mut server, &mut client, FIRST_DATAGRAM);
+    send_datagram(&mut server, &mut client, SECOND_DATAGRAM);
+    send_datagram(&mut server, &mut client, THIRD_DATAGRAM);
 
     // Datagram with FIRST_DATAGRAM data will be dropped.
     assert!(matches!(
@@ -440,7 +442,7 @@ fn too_many_datagram_events() {
     assert_eq!(client.stats().incoming_datagram_dropped, 1);
 
     // New events can be queued.
-    send_datagram(&mut client, &mut server, FOURTH_DATAGRAM);
+    send_datagram(&mut server, &mut client, FOURTH_DATAGRAM);
     assert!(matches!(
         client.next_event().unwrap(),
         ConnectionEvent::Datagram(data) if data == FOURTH_DATAGRAM
@@ -469,4 +471,64 @@ fn multiple_quic_datagrams_in_one_packet() {
     server.process_input(out.unwrap(), now());
     let datagram = |e: &_| matches!(e, ConnectionEvent::Datagram(..));
     assert_eq!(server.events().filter(datagram).count(), 2);
+}
+
+/// Datagrams that are close to the capacity of the packet need special
+/// handling.  They need to use the packet-filling frame type and
+/// they cannot allow other frames to follow.
+#[test]
+fn datagram_fill() {
+    struct PanickingFrameWriter {}
+    impl crate::connection::test_internal::FrameWriter for PanickingFrameWriter {
+        fn write_frames(&mut self, builder: &mut PacketBuilder) {
+            panic!(
+                "builder invoked with {} bytes remaining",
+                builder.remaining()
+            );
+        }
+    }
+    struct TrackingFrameWriter {
+        called: Rc<RefCell<bool>>,
+    }
+    impl crate::connection::test_internal::FrameWriter for TrackingFrameWriter {
+        fn write_frames(&mut self, builder: &mut PacketBuilder) {
+            assert_eq!(builder.remaining(), 2);
+            *self.called.borrow_mut() = true;
+        }
+    }
+
+    let (mut client, mut server) = connect_datagram();
+
+    // Work out how much space we have for a datagram.
+    let space = {
+        let p = client.paths.primary();
+        let path = p.borrow();
+        // Minimum overhead is connection ID length, 1 byte short header, 1 byte packet number,
+        // 1 byte for the DATAGRAM frame type, and 16 bytes for the AEAD.
+        path.mtu() - path.remote_cid().len() - 19
+    };
+    assert!(space >= 64); // Unlikely, but this test depends on the datagram being this large.
+
+    // This should not be called.
+    client.test_frame_writer = Some(Box::new(PanickingFrameWriter {}));
+
+    let buf = vec![9; space];
+    // This will completely fill available space.
+    send_datagram(&mut client, &mut server, &buf);
+    // This will leave 1 byte free, but more frames won't be added in this space.
+    send_datagram(&mut client, &mut server, &buf[..buf.len() - 1]);
+    // This will leave 2 bytes free, which is enough space for a length field,
+    // but not enough space for another frame after that.
+    send_datagram(&mut client, &mut server, &buf[..buf.len() - 2]);
+    // Three bytes free will be space enough for a length frame, but not enough
+    // space left over for another frame (we need 2 bytes).
+    send_datagram(&mut client, &mut server, &buf[..buf.len() - 3]);
+
+    // Four bytes free is enough space for another frame.
+    let called = Rc::new(RefCell::new(false));
+    client.test_frame_writer = Some(Box::new(TrackingFrameWriter {
+        called: Rc::clone(&called),
+    }));
+    send_datagram(&mut client, &mut server, &buf[..buf.len() - 4]);
+    assert!(*called.borrow());
 }
