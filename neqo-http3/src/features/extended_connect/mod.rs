4988f45f3d416c67e5d37d7e744478917174ec3c
Dragana Damjanovic <dragana.damjano@gmail.com>
2021-12-21 01:28:26 +0100

    Make WebTransportSession as a send and recv stream that holds Recv/SendMessage.
    ExtentedConnectSession was renamed to WebTransportSession. This was not a proper generalization of extended connections. Additional extended connect will need a separate implementation, e.g. WebTransport. We should make a generalization as needed.  Something like Masque has a potential to reuse functionality of WebTransportSession, but let’s wait for an implementation to make WebTransportSession generic.
    
    In the current implementation ExtenedConnectSession was only a listener to the underlying http stream (Recv/SendMessage). This was working fine until the need for reading the body of the http stream was introduced. Since listeners cannot read data from a http stream, the task was done by the Htttp3Connection(e.g. function read_webtransport_control_stream).
    This patch creates WebTransportSession’s Recv/SendStream that holds the http Recv/SendMessage. All reads and writes are done in the WebTransportSession and Http3Connection does not need to know about WebTransport details. There are no more special reads for the WebTransport streams.

--- neqo-http3/src/features/extended_connect/mod.rs
+++ neqo-http3/src/features/extended_connect/mod.rs
@@ -6,20 +6,16 @@
 
 #![allow(clippy::module_name_repetitions)]
 
-pub mod session;
-pub mod webtransport;
+pub mod webtransport_session;
+pub mod webtransport_streams;
 
 use crate::client_events::Http3ClientEvents;
 use crate::features::NegotiationState;
 use crate::settings::{HSettingType, HSettings};
-use crate::{CloseType, Error, Http3StreamInfo, Http3StreamType, RecvStream, Res, SendStream};
-use neqo_transport::{AppError, Connection, StreamId};
-pub use session::ExtendedConnectSession;
-use std::cell::RefCell;
-use std::collections::BTreeSet;
-use std::collections::HashMap;
+use crate::{CloseType, Http3StreamInfo, Http3StreamType};
+use neqo_transport::{AppError, StreamId};
 use std::fmt::Debug;
-use std::rc::Rc;
+pub use webtransport_session::WebTransportSession;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum SessionCloseReason {
@@ -88,7 +84,6 @@ impl From<ExtendedConnectType> for HSettingType {
 #[derive(Debug)]
 pub struct ExtendedConnectFeature {
     feature_negotiation: NegotiationState,
-    sessions: HashMap<StreamId, Rc<RefCell<ExtendedConnectSession>>>,
 }
 
 impl ExtendedConnectFeature {
@@ -96,27 +91,11 @@ impl ExtendedConnectFeature {
     pub fn new(connect_type: ExtendedConnectType, enable: bool) -> Self {
         Self {
             feature_negotiation: NegotiationState::new(enable, HSettingType::from(connect_type)),
-            sessions: HashMap::new(),
         }
     }
 
     pub fn set_listener(&mut self, new_listener: Http3ClientEvents) {
         self.feature_negotiation.set_listener(new_listener);
-    }
-
-    pub fn insert(&mut self, stream_id: StreamId, session: Rc<RefCell<ExtendedConnectSession>>) {
-        self.sessions.insert(stream_id, session);
-    }
-
-    pub fn get_session(
-        &mut self,
-        stream_id: StreamId,
-    ) -> Option<Rc<RefCell<ExtendedConnectSession>>> {
-        if !matches!(self.feature_negotiation, NegotiationState::Negotiated) {
-            return None;
-        }
-
-        self.sessions.get_mut(&stream_id).cloned()
     }
 
     pub fn handle_settings(&mut self, settings: &HSettings) {
@@ -126,44 +105,5 @@ impl ExtendedConnectFeature {
     #[must_use]
     pub fn enabled(&self) -> bool {
         self.feature_negotiation.enabled()
-    }
-
-    pub fn remove(
-        &mut self,
-        stream_id: StreamId,
-    ) -> Option<(BTreeSet<StreamId>, BTreeSet<StreamId>)> {
-        self.sessions
-            .remove(&stream_id)
-            .and_then(|ec| ec.borrow_mut().take_sub_streams())
-    }
-
-    /// # Errors
-    /// It may return an error if the frame is not correctly decoded.
-    pub fn read_control_stream(
-        &mut self,
-        stream_id: StreamId,
-        recv_stream: &mut Box<dyn RecvStream>,
-        conn: &mut Connection,
-    ) -> Res<bool> {
-        if let Some(s) = self.sessions.get(&stream_id) {
-            return s.borrow_mut().read_control_stream(recv_stream, conn);
-        }
-        Ok(false)
-    }
-
-    /// # Errors
-    /// Return an error if the stream was closed on the transport layer, but that information is not yet
-    /// consumed on the http/3 layer.
-    pub fn close_session(
-        &mut self,
-        stream_id: StreamId,
-        send_stream: &mut Box<dyn SendStream>,
-        conn: &mut Connection,
-        error: u32,
-        message: &str,
-    ) -> Res<()> {
-        self.sessions.get(&stream_id).map_or(Err(Error::InvalidStreamId), |s| 
-            s.borrow_mut()
-                .close_session(send_stream, conn, error, message))
     }
 }
