3a91e17ae81271509245786d7cbd80883a1940a0
Martin Thomson <mt@lowentropy.net>
2023-01-11 15:00:26 +1100

    Format

--- neqo-transport/src/recovery.rs
+++ neqo-transport/src/recovery.rs
@@ -342,8 +342,7 @@ impl LossRecoverySpace {
     /// and when keys are dropped.
     fn remove_ignored(&mut self) -> impl Iterator<Item = SentPacket> {
         self.in_flight_outstanding = 0;
-        mem::take(&mut self.sent_packets)
-            .into_values()
+        mem::take(&mut self.sent_packets).into_values()
     }
 
     /// Remove the primary path marking on any packets this is tracking.
