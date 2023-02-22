13f5cd80e4095b65224e364a6aaa7cb778b21b74
Martin Thomson <mt@lowentropy.net>
2023-02-22 14:31:42 +1100

    Eliminate use of `#[macro_use]`
    This is a relic of an older rust edition.  Macros are available to the
    crate in which they are defined and used like functions otherwise.

--- neqo-common/src/log.rs
+++ neqo-common/src/log.rs
@@ -11,6 +11,8 @@ use lazy_static::lazy_static;
 use std::io::Write;
 use std::sync::Once;
 use std::time::Instant;
+use env_logger::Builder;
+use lazy_static::lazy_static;
 
 #[macro_export]
 macro_rules! do_log {
