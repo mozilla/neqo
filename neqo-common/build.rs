2a1b774249f57bcd30cb248699034099a7911727
Lars Eggert <lars@eggert.org>
2024-01-02 14:02:31 +0200

    cfg

--- neqo-common/build.rs
+++ neqo-common/build.rs
@@ -1,8 +1,13 @@
+use cfg_aliases::cfg_aliases;
 use std::env;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("windows") {
         println!("cargo:rustc-link-lib=winmm");
+    }
+
+    cfg_aliases! {
+        posix_socket: { any(target_os = "macos", target_os = "linux", target_os = "android") },
     }
 }
