b2011884a80bd11723f12bf1d6681c2c3c749588
Lars Eggert <lars@eggert.org>
2024-02-01 16:23:13 +0200

    cargo fmt

--- neqo-transport/benches/range_tracker.rs
+++ neqo-transport/benches/range_tracker.rs
@@ -7,23 +7,29 @@ fn build_coalesce(len: u64) -> RangeTracker {
     used.mark_range(1000, 100000, RangeState::Sent);
     // leave a gap or it will coalesce here
     for i in 2..=len {
-	// These do not get immediately coalesced when marking since they're not at the end or start
-	used.mark_range(i * 1000, 1000, RangeState::Acked); 
+        // These do not get immediately coalesced when marking since they're not at the end or start
+        used.mark_range(i * 1000, 1000, RangeState::Acked);
     }
     return used;
 }
 
 fn coalesce(used: &mut RangeTracker) {
-    used.mark_range(1000,1000, RangeState::Acked);
+    used.mark_range(1000, 1000, RangeState::Acked);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut used = build_coalesce(1);
-    c.bench_function("coalesce_acked_from_zero 2 entries", |b| b.iter(|| coalesce(&mut used)));
+    c.bench_function("coalesce_acked_from_zero 2 entries", |b| {
+        b.iter(|| coalesce(&mut used))
+    });
     used = build_coalesce(100);
-    c.bench_function("coalesce_acked_from_zero 100 entries", |b| b.iter(|| coalesce(&mut used)));
+    c.bench_function("coalesce_acked_from_zero 100 entries", |b| {
+        b.iter(|| coalesce(&mut used))
+    });
     used = build_coalesce(1000);
-    c.bench_function("coalesce_acked_from_zero 1000 entries", |b| b.iter(|| coalesce(&mut used)));
+    c.bench_function("coalesce_acked_from_zero 1000 entries", |b| {
+        b.iter(|| coalesce(&mut used))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- neqo-transport/src/lib.rs
+++ neqo-transport/src/lib.rs
@@ -181,7 +181,7 @@ impl From<std::num::TryFromIntError> for Error {
 }
 
 impl ::std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn::std::error::Error + 'static)> {
+    fn source(&self) -> Option<&(dyn ::std::error::Error + 'static)> {
         match self {
             Self::CryptoError(e) => Some(e),
             _ => None,
