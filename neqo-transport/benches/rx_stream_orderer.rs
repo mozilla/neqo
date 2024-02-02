45c93397455635aa99bffdfa094ce2c5c1446a66
Lars Eggert <lars@eggert.org>
2024-02-02 10:19:25 +0200

    Address various code review comments

--- neqo-transport/benches/rx_stream_orderer.rs
+++ neqo-transport/benches/rx_stream_orderer.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion}; // black_box
+use criterion::{criterion_group, criterion_main, Criterion};
 use neqo_transport::recv_stream::RxStreamOrderer;
 
 fn rx_stream_orderer() {
@@ -6,13 +6,13 @@ fn rx_stream_orderer() {
     let data: &[u8] = &[0; 1337];
 
     for i in 0..100000 {
-        rx.inbound_frame(i * 1337, &data);
+        rx.inbound_frame(i * 1337, data);
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("RxStreamOrderer::inbound_frame()", |b| {
-        b.iter(|| rx_stream_orderer())
+        b.iter(rx_stream_orderer)
     });
 }
 
