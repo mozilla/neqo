e9ba397891451563e4c5674a0949ee060e8a83d7
Max Inden <mail@max-inden.de>
2024-02-20 13:22:36 +0100

    Test with Ect1 instead of Ce
    Windows does not allow setting Ce:
    
    > Your application isn't allowed to specify the Congestion Encountered (CE) code point when sending datagrams. The send will return with error WSAEINVAL.
    
    https://learn.microsoft.com/en-us/windows/win32/winsock/winsock-ecn

--- neqo-common/src/udp.rs
+++ neqo-common/src/udp.rs
@@ -137,7 +137,7 @@ mod tests {
 
         sleep(Duration::from_millis(100)).await;
 
-        let tos_tx = IpTos::from((IpTosDscp::Le, IpTosEcn::Ce));
+        let tos_tx = IpTos::from((IpTosDscp::Le, IpTosEcn::Ect1));
         let datagram = Datagram::new(
             sender.local_addr()?,
             receiver.local_addr()?,
