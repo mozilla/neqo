2ed1ecb2e35a06fcf1eb34dbb999db3895c2af65
Lars Eggert <lars@eggert.org>
2024-03-27 12:09:31 +0200

    Fix clippy

--- neqo-transport/src/frame.rs
+++ neqo-transport/src/frame.rs
@@ -406,7 +406,6 @@ impl<'a> Frame<'a> {
     /// # Errors
     ///
     /// Returns an error if the frame cannot be decoded.
-    ///
     #[allow(clippy::too_many_lines)] // Yeah, but it's a nice match statement.
     pub fn decode(dec: &mut Decoder<'a>) -> Res<Self> {
         /// Maximum ACK Range Count in ACK Frame
