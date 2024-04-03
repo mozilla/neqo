01c9b6357dee3229934cb41eb51ceb741fe678bf
Max Inden <mail@max-inden.de>
2024-04-03 10:12:59 +0200

    Review suggestions

--- fuzz/fuzz_targets/packet.rs
+++ fuzz/fuzz_targets/packet.rs
@@ -5,24 +5,16 @@ use libfuzzer_sys::fuzz_target;
 
 #[cfg(not(windows))]
 fuzz_target!(|data: &[u8]| {
-    use std::sync::Once;
-
     use neqo_transport::{packet::PublicPacket, RandomConnectionIdGenerator};
 
-    static INIT: Once = Once::new();
-    static mut DECODER: Option<neqo_transport::RandomConnectionIdGenerator> = None;
-
-    // Initialize things
-    unsafe {
-        INIT.call_once(|| {
-            neqo_crypto::init();
-            DECODER = Some(RandomConnectionIdGenerator::new(20));
-        });
+    lazy_static::lazy_static! {
+        static ref DECODER: RandomConnectionIdGenerator = RandomConnectionIdGenerator::new(20);
     }
-    let decoder = unsafe { DECODER.as_ref().unwrap() };
+
+    neqo_crypto::init();
 
     // Run the fuzzer
-    let _ = PublicPacket::decode(data, decoder);
+    let _ = PublicPacket::decode(data, &*DECODER);
 });
 
 #[cfg(windows)]
