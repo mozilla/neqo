23b09ec1308d7dee931804cd7281cac96b5259d6
Lars Eggert <lars@eggert.org>
2024-04-04 09:03:22 +0300

    `lazy_static` -> `OnceLock`

--- fuzz/fuzz_targets/packet.rs
+++ fuzz/fuzz_targets/packet.rs
@@ -5,16 +5,16 @@ use libfuzzer_sys::fuzz_target;
 
 #[cfg(not(windows))]
 fuzz_target!(|data: &[u8]| {
+    use std::sync::OnceLock;
+
     use neqo_transport::{packet::PublicPacket, RandomConnectionIdGenerator};
 
-    lazy_static::lazy_static! {
-        static ref DECODER: RandomConnectionIdGenerator = RandomConnectionIdGenerator::new(20);
-    }
-
+    static DECODER: OnceLock<RandomConnectionIdGenerator> = OnceLock::new();
+    let decoder = DECODER.get_or_init(|| RandomConnectionIdGenerator::new(20));
     neqo_crypto::init().unwrap();
 
     // Run the fuzzer
-    let _ = PublicPacket::decode(data, &*DECODER);
+    let _ = PublicPacket::decode(data, decoder);
 });
 
 #[cfg(windows)]
