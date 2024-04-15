fae33f62ebc935f1dbb2851973d31c5c05325a35
Lars Eggert <lars@eggert.org>
2024-04-15 19:51:26 +0300

    Update qlog.rs
    Co-authored-by: Max Inden <mail@max-inden.de>
    Signed-off-by: Lars Eggert <lars@eggert.org>

--- neqo-transport/src/qlog.rs
+++ neqo-transport/src/qlog.rs
@@ -390,8 +390,6 @@ pub fn metrics_updated(qlog: &mut NeqoQlog, updated_metrics: &[QlogMetric]) {
 impl From<&Frame<'_>> for QuicFrame {
     fn from(frame: &Frame) -> Self {
         match frame {
-            // TODO: Add payload length to `QuicFrame::Padding` once
-            // https://github.com/cloudflare/quiche/pull/1745 is available via the qlog crate.
             Frame::Padding(len) => QuicFrame::Padding {
                 length: None,
                 payload_length: u32::from(*len),
