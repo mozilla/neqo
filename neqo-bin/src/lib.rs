705227436c4d9996defee13f2bbeef61a4cd29f3
Max Inden <mail@max-inden.de>
2024-04-16 21:15:07 +0200

    Disable pacing

--- neqo-bin/src/lib.rs
+++ neqo-bin/src/lib.rs
@@ -133,7 +133,7 @@ impl Default for QuicParameters {
             max_streams_uni: 16,
             idle_timeout: 30,
             congestion_control: CongestionControlAlgorithm::NewReno,
-            no_pacing: false,
+            no_pacing: true,
             preferred_address_v4: None,
             preferred_address_v6: None,
         }
