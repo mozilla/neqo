6e6aafaf21abac59a7b370753189cbc29f7784e2
Lars Eggert <lars@eggert.org>
2024-07-18 19:48:01 +0300

    Roll back

--- neqo-transport/src/recovery/mod.rs
+++ neqo-transport/src/recovery/mod.rs
@@ -739,7 +739,7 @@ impl LossRecovery {
         let rtt = path.rtt();
         let loss_time = self.earliest_loss_time(rtt);
         let pto_time =
-            if path.amplification_limit() > ACK_ONLY_SIZE_LIMIT || self.confirmed_time.is_some() {
+            if path.amplification_limit() > path.plpmtu() {
                 self.earliest_pto(rtt)
             } else {
                 None
