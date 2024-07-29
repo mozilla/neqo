a5a5ecefe3be2a2bd42e4931e5ccdbcecffe4af6
John M. Schanck <jschanck@mozilla.com>
2024-07-29 11:40:45 -0700

    handle len == 0 case in null_safe_slice

--- neqo-crypto/src/lib.rs
+++ neqo-crypto/src/lib.rs
@@ -217,12 +217,11 @@ unsafe fn null_safe_slice<'a, T, L>(data: *const T, len: L) -> &'a [T]
 where
     usize: TryFrom<L>,
 {
-    if data.is_null() {
+    let len = usize::try_from(len).unwrap_or_else(|_| panic!("null_safe_slice: size overflow"));
+    if data.is_null() || len == 0 {
         &[]
-    } else if let Ok(len) = usize::try_from(len) {
+    } else {
         #[allow(clippy::disallowed_methods)]
         std::slice::from_raw_parts(data, len)
-    } else {
-        panic!("null_safe_slice: size overflow");
     }
 }
