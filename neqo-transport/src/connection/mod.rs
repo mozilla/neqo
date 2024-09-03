dbfc372492f517f0a0a9c202c7c266d30a4aeae7
Max Inden <mail@max-inden.de>
2024-09-03 20:16:30 +0200

    perf(transport): don't pre-allocate mtu on max_datagram_size
    The `neqo_transport::Connection::max_datagram_size` creates an `Encoder`, writes
    a packet header and a packet number and determines how many bytes of the mtu are left.
    
    https://github.com/mozilla/neqo/blob/28f60bd0ba3209ecba4102eec123859a3a8afd45/neqo-transport/src/connection/mod.rs#L3408-L3427
    
    The `Encoder` only has to hold the packet header and the packet number. Yet it
    is initialized with `Encoder::with_capacity(mtu)`.
    
    https://github.com/mozilla/neqo/blob/28f60bd0ba3209ecba4102eec123859a3a8afd45/neqo-transport/src/connection/mod.rs#L3408
    
    Note that `PacketBuilder::short` and `PacketBuilder::long` read the
    `Encoder::capacity` through `PacketBuilder::infer_limit`. But
    `PacketBuilder::infer_limit` falls back to `2048` if the capacity is below `64`,
    which will be the case when using `Encoder::default()` instead of
    `Encoder::with_capacity(mtu)`. `2048` should be plenty enough for the packet
    header and the packet number.
    
    https://github.com/mozilla/neqo/blob/28f60bd0ba3209ecba4102eec123859a3a8afd45/neqo-transport/src/packet/mod.rs#L152-L180
    
    https://github.com/mozilla/neqo/blob/28f60bd0ba3209ecba4102eec123859a3a8afd45/neqo-transport/src/packet/mod.rs#L188-L225
    
    https://github.com/mozilla/neqo/blob/28f60bd0ba3209ecba4102eec123859a3a8afd45/neqo-transport/src/packet/mod.rs#L135-L141
    
    This commit prevents the wasted allocation by using `Encoder::default()` instead
    of `Encoder::with_capacity(mtu)`.

--- neqo-transport/src/connection/mod.rs
+++ neqo-transport/src/connection/mod.rs
@@ -3395,7 +3395,7 @@ impl Connection {
         };
         let path = self.paths.primary().ok_or(Error::NotAvailable)?;
         let mtu = path.borrow().plpmtu();
-        let encoder = Encoder::with_capacity(mtu);
+        let encoder = Encoder::default();
 
         let (_, mut builder) = Self::build_packet_header(
             &path.borrow(),
