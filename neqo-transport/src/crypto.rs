218dec8fc253717537a34c77f6677a3122402a70
Lars Eggert <lars@eggert.org>
2024-11-15 10:41:18 +0200

    More tests and corner-case fixes

--- neqo-transport/src/crypto.rs
+++ neqo-transport/src/crypto.rs
@@ -1361,10 +1361,16 @@ pub enum CryptoStreams {
 }
 
 /// [Fisher–Yates shuffle](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modern_method)
-/// Modified to make sure no element stays in place.
+/// Modified to make sure no element stays in place if `a` has more than one element.
+///
+/// TODO: We could use <https://docs.rs/rand/latest/rand/seq/trait.SliceRandom.html#tymethod.shuffle>
+/// instead, but I assume we want to only use the NSS RNG in neqo?
 fn shuffle<T>(a: &mut [T]) {
     // To shuffle an array a of n elements (indices 0..n-1)
     let n: usize = a.len();
+    if n < 2 {
+        return;
+    }
     for i in 0..(n - 1) {
         // j ← random integer such that i ≤ j < n
         let j = loop {
@@ -1631,4 +1637,134 @@ pub struct CryptoRecoveryToken {
     space: PacketNumberSpace,
     offset: u64,
     length: usize,
+}
+
+#[cfg(test)]
+mod tests {
+    use test_fixture::fixture_init;
+
+    #[test]
+    fn shuffle() {
+        fixture_init();
+
+        // Empty arrays should remain empty.
+        let mut a: [i32; 0] = [];
+        let b = a;
+        super::shuffle(&mut a);
+        assert_eq!(a, b);
+
+        // For a one-element array, the only possible shuffle is the identity.
+        let mut a = [1];
+        let b = a;
+        super::shuffle(&mut a);
+        assert_eq!(a, b);
+
+        // For a two-element array, the only possible shuffle is the reverse, since `shuffle`
+        // doesn't leave any element in place.
+        let mut a = [1, 2];
+        let mut b = a;
+        super::shuffle(&mut a);
+        b.reverse();
+        assert_eq!(a, b);
+
+        // For three-element and longer arrays, the shuffle is always different from the original.
+        let mut a = [1, 2, 3];
+        let b = a;
+        super::shuffle(&mut a);
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    fn ascii_sequences() {
+        const N: usize = 3;
+
+        // Empty input
+        let data = b"";
+        let sequences = super::ascii_sequences(data, N);
+        assert!(sequences.is_empty());
+
+        // No graphic ASCII
+        let data = b"\x00\x01\x02";
+        let sequences = super::ascii_sequences(data, N);
+        assert!(sequences.is_empty());
+
+        // Sequences shorter than required length
+        let data = b"ab\x00cd";
+        let sequences = super::ascii_sequences(data, N);
+        assert!(sequences.is_empty());
+
+        // One valid sequence of the required length
+        let data = b"abc";
+        let sequences = super::ascii_sequences(data, N);
+        assert_eq!(sequences, vec![(0, 3)]);
+
+        // Multiple valid sequences
+        let data = b"abc\x00defgh\x00ijklmno";
+        let sequences = super::ascii_sequences(data, N);
+        assert_eq!(sequences, vec![(0, 3), (4, 9), (10, 17)]);
+
+        // One sequence at the end of data
+        let data = b"\x00\x00abcde";
+        let sequences = super::ascii_sequences(data, N);
+        assert_eq!(sequences, vec![(2, 7)]);
+
+        // One sequence at the beginning of data
+        let data = b"abcde\x00\x00";
+        let sequences = super::ascii_sequences(data, N);
+        assert_eq!(sequences, vec![(0, 5)]);
+    }
+
+    #[test]
+    fn reorder_chunks() {
+        // Test that all data is included in the chunks
+        fn assert_complete(data: &[u8], chunks: &[(u64, &[u8])]) {
+            // Test that the chunk lengths sum to the total length
+            let total_length: usize = chunks.iter().map(|(_, chunk)| chunk.len()).sum();
+            assert_eq!(total_length, data.len());
+
+            // Test that the combined chunks cover the entire data
+            let mut reconstructed = vec![0; data.len()];
+            for &(index, data) in chunks {
+                let idx: usize = index.try_into().unwrap();
+                reconstructed.splice(idx..idx + data.len(), data.iter().copied());
+            }
+            assert_eq!(data, reconstructed.as_slice());
+        }
+
+        fixture_init();
+
+        // Empty input -> empty output
+        let data = b"";
+        let chunks = super::reorder_chunks(data);
+        assert_eq!(chunks, vec![(0, data.as_ref())]);
+        assert_complete(data, &chunks);
+
+        // Data without graphic ASCII sequences -> output == input
+        let data = b"\x00\x01\x02";
+        let chunks = super::reorder_chunks(data);
+        assert_eq!(chunks, vec![(0, data.as_ref())]);
+        assert_complete(data, &chunks);
+
+        // Data containing one graphic ASCII sequence -> one predictable reordering
+        let data = b"abc";
+        let chunks = super::reorder_chunks(data);
+        assert_eq!(chunks, vec![(1, &data[1..3]), (0, &data[0..1])]);
+        assert_complete(data, &chunks);
+
+        // Data containing two graphic ASCII sequences -> one of two predictable reorderings
+        let data = b"abc\x00def";
+        let chunks = super::reorder_chunks(data);
+        assert_eq!(chunks.len(), 3); // 2 splits create 3 chunks
+        let order1 = [(1, &data[1..5]), (5, &data[5..7]), (0, &data[0..1])];
+        let order2 = [(5, &data[5..7]), (0, &data[0..1]), (1, &data[1..5])];
+        assert!(chunks == order1 || chunks == order2);
+        assert_complete(data, &chunks);
+
+        // Data containing three graphic ASCII sequences
+        let data = b"abc\x00defg\x00hijkl";
+        let chunks = super::reorder_chunks(data);
+        // Too many possibilities to check, just check that the output is valid
+        assert_eq!(chunks.len(), 4); // 3 splits create 4 chunks
+        assert_complete(data, &chunks);
+    }
 }
