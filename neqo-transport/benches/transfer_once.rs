6dd3829f3b16a86411c7a7255e967ef678dac36d
Max Inden <mail@max-inden.de>
2024-12-02 20:19:36 +0100

    Move to benchmark

---
+++ neqo-transport/benches/transfer_once.rs
@@ -0,0 +1,57 @@
+use std::{ops::Range, time::Duration};
+
+use neqo_transport::{CloseReason, ConnectionParameters, Error, State};
+use test_fixture::{
+    boxed,
+    sim::{
+        connection::{ConnectionNode, ReachState, ReceiveData, SendData},
+        network::{Delay, Drop, NonRandomDelay, TailDrop},
+        Simulator,
+    },
+    simulate,
+};
+
+#[allow(clippy::cast_precision_loss)]
+pub fn main() {
+    const MIB: usize = 1024 * 1024;
+    const TRANSFER_AMOUNT: usize = 1000 * MIB;
+
+    let sim = Simulator::new(
+        "gbit-bandwidth",
+        boxed![
+            ConnectionNode::new_client(
+                ConnectionParameters::default().pmtud(false).pacing(true),
+                boxed![ReachState::new(State::Confirmed)],
+                boxed![ReceiveData::new(TRANSFER_AMOUNT)]
+            ),
+            TailDrop::gbit_link(),
+            NonRandomDelay::new(Duration::from_millis(20)),
+            ConnectionNode::new_server(
+                ConnectionParameters::default().pmtud(false).pacing(true),
+                boxed![ReachState::new(State::Confirmed)],
+                boxed![SendData::new(TRANSFER_AMOUNT)]
+            ),
+            TailDrop::gbit_link(),
+            NonRandomDelay::new(Duration::from_millis(20)),
+        ],
+    );
+
+    let simulated_time = sim.setup().run();
+    let bandwidth = TRANSFER_AMOUNT as f64 * 8.0 / simulated_time.as_secs_f64();
+
+    let maximum_bandwidth = 1_000_000_000.0; // bandwidth-delay-product / delay = bandwidth
+    let expected_utilization = 0.5;
+
+    println!(
+        "expected to reach {expected_utilization} of maximum bandwidth ({} Mbit/s) but got {} Mbit/s",
+        maximum_bandwidth / MIB as f64,
+        bandwidth  / MIB as f64,
+    );
+
+    assert!(
+        maximum_bandwidth * expected_utilization < bandwidth,
+        "expected to reach {expected_utilization} of maximum bandwidth ({} Mbit/s) but got {} Mbit/s",
+        maximum_bandwidth / MIB as f64,
+        bandwidth  / MIB as f64,
+    );
+}
