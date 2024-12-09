7d42973cdb9ec7b899c9cd95539c6f0b91554ceb
Lars Eggert <lars@eggert.org>
2024-12-09 20:59:53 +0200

    chore: Simplify some webtransport checks (#2267)
    * chore: Simplify some webtransport checks
    
    So this just uses it.
    
    While I'm here, eliminate some code redundancies.
    
    Fixes #1255
    
    * Add some helper functions for headers
    
    This should reduce the cost of searching headers a little.
    
    ---------
    
    Co-authored-by: Martin Thomson <mt@lowentropy.net>

--- neqo-http3/src/features/extended_connect/tests/webtransport/mod.rs
+++ neqo-http3/src/features/extended_connect/tests/webtransport/mod.rs
@@ -10,7 +10,7 @@ mod sessions;
 mod streams;
 use std::{cell::RefCell, rc::Rc, time::Duration};
 
-use neqo_common::event::Provider;
+use neqo_common::{event::Provider, header::HeadersExt};
 use neqo_crypto::AuthenticationStatus;
 use neqo_transport::{ConnectionParameters, Pmtud, StreamId, StreamType};
 use test_fixture::{
@@ -58,6 +58,13 @@ pub fn default_http3_server(server_params: Http3Parameters) -> Http3Server {
         None,
     )
     .expect("create a server")
+}
+
+pub fn assert_wt(headers: &[Header]) {
+    assert!(
+        headers.contains_header(":method", "CONNECT")
+            && headers.contains_header(":protocol", "webtransport")
+    );
 }
 
 fn exchange_packets(client: &mut Http3Client, server: &mut Http3Server) {
@@ -148,14 +155,7 @@ impl WtTest {
                     session,
                     headers,
                 }) => {
-                    assert!(
-                        headers
-                            .iter()
-                            .any(|h| h.name() == ":method" && h.value() == "CONNECT")
-                            && headers
-                                .iter()
-                                .any(|h| h.name() == ":protocol" && h.value() == "webtransport")
-                    );
+                    assert_wt(&headers);
                     session.response(accept).unwrap();
                     wt_server_session = Some(session);
                 }
@@ -183,7 +183,7 @@ impl WtTest {
                 }) if (
                     stream_id == wt_session_id &&
                     status == 200 &&
-                    headers.contains(&Header::new(":status", "200"))
+                    headers.contains_header(":status", "200")
                 )
             )
         };
