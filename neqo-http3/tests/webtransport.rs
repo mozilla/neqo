6f92d112ea8e9baf48e911ce147a08d847861ee6
Lars Eggert <lars@eggert.org>
2024-12-09 12:36:25 +0200

    Update neqo-http3/tests/webtransport.rs
    Co-authored-by: Max Inden <mail@max-inden.de>
    Signed-off-by: Lars Eggert <lars@eggert.org>

--- neqo-http3/tests/webtransport.rs
+++ neqo-http3/tests/webtransport.rs
@@ -234,6 +234,7 @@ fn wt_keepalive() {
     let (mut client, mut server) = connect();
     let _wt_session = create_wt_session(&mut client, &mut server);
     let idle_timeout = ConnectionParameters::default().get_idle_timeout();
+    // Expect client and server to send PING after half of the idle timeout in order to keep connection alive.
     assert_eq!(client.process_output(now()).callback(), idle_timeout / 2);
     assert_eq!(server.process_output(now()).callback(), idle_timeout / 2);
 }
