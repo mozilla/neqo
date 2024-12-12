5e9054cb9d20c1dce76f8b78c29c502f91a26e3f
Lars Eggert <lars@eggert.org>
2024-12-12 16:15:27 +0200

    Fix test

--- neqo-transport/src/connection/tests/stream.rs
+++ neqo-transport/src/connection/tests/stream.rs
@@ -684,7 +684,8 @@ fn late_stream_related_frame(frame_type: u64) {
 
     // Now clear the streams on the client, and then deliver the test frame.
     client.streams.clear_streams();
-    assert!(client.obtain_stream(stream_id).unwrap().is_none());
+    let (ss, rs) = client.streams.obtain_stream(stream_id).unwrap();
+    assert!(ss.is_none() && rs.is_none());
     _ = client.process(tester, now()).dgram();
 
     // Make sure this worked, i.e., the connection didn't close.
