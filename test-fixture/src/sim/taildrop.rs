569c5fa00edf15a59a481b999b6b8bd4135dc630
Max Inden <mail@max-inden.de>
2024-12-29 19:48:59 +0100

    Remove debugging assert

--- test-fixture/src/sim/taildrop.rs
+++ test-fixture/src/sim/taildrop.rs
@@ -112,7 +112,6 @@ impl TailDrop {
         let send_ns = u64::try_from(t >> 32).unwrap();
         assert_ne!(send_ns, 0, "sending a packet takes <1ns");
         self.sub_ns_delay = u32::try_from(t & u128::from(u32::MAX)).unwrap();
-        assert!(send_ns < Duration::from_millis(1).as_nanos() as u64);
         let deque_time = now + Duration::from_nanos(send_ns);
         self.next_deque = Some(deque_time);
 
