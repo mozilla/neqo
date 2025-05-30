e33540f10a3d59a8009add2ba87adc9208616316
Lars Eggert <lars@eggert.org>
2025-05-30 17:02:40 +0300

    Finalize

--- neqo-common/benches/decoder.rs
+++ neqo-common/benches/decoder.rs
@@ -54,7 +54,7 @@ fn benchmark_decoder(c: &mut Criterion) {
 
 criterion_group! {
     name = benches;
-    config = Criterion::default(); //.warm_up_time(Duration::from_secs(5)).measurement_time(Duration::from_secs(60));
+    config = Criterion::default().warm_up_time(Duration::from_secs(5)).measurement_time(Duration::from_secs(60));
     targets = benchmark_decoder
 }
 criterion_main!(benches);
