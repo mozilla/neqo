9279e798ee287e0174717033bdd0a74bf1a516d4
Martin Thomson <mt@lowentropy.net>
2025-08-18 14:55:06 +1000

    Move test_frame_writer use
    ... to ensure that it can be used for handshake packet types.
    
    Takes @larseggert's suggestion to assert that the packet isn't full.

--- neqo-transport/src/connection/mod.rs
+++ neqo-transport/src/connection/mod.rs
@@ -2379,11 +2379,6 @@ impl Connection {
 
         self.streams
             .write_frames(TransmissionPriority::Low, builder, tokens, frame_stats);
-
-        #[cfg(test)]
-        if let Some(w) = &mut self.test_frame_writer {
-            w.write_frames(builder);
-        }
     }
 
     // Maybe send a probe.  Return true if the packet was ack-eliciting.
@@ -2507,6 +2502,12 @@ impl Connection {
                     &mut tokens,
                     stats,
                 );
+            }
+
+            #[cfg(test)]
+            if let Some(w) = &mut self.test_frame_writer {
+                assert!(!builder.is_full(), "test_frame_writer set on full packet");
+                w.write_frames(builder);
             }
         }
 
