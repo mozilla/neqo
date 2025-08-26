fc4cd24652a240a6a3765d5c83010536bec31112
Max Leonard Inden <mail@max-inden.de>
2025-08-26 16:24:53 +0200

    fix(transport/bench): use new group for each benchmark
    We parse the criterion benchmark output, alter it, then post it as a
    GitHub comment.
    
    https://github.com/mozilla/neqo/blob/8ce7f9bcdef89fa5a1eb37db6a2375db9a4531ea/.github/workflows/bench.yml#L163-L187
    
    We want to split by benchmark. The easiest way to do so is through a
    newline. Criterion only inserts a newline for each benchmark group. Thus
    split the existing group into two, one per benchmark function (i.e.
    wallclock-time and simulated-time).
    
    Issue raised in https://github.com/mozilla/neqo/pull/2885#issuecomment-3223885666

--- neqo-transport/benches/transfer.rs
+++ neqo-transport/benches/transfer.rs
@@ -57,39 +57,41 @@ fn benchmark_transfer(c: &mut Criterion, label: &str, seed: Option<impl AsRef<st
             sim.setup()
         };
 
-        let mut group = c.benchmark_group(format!("transfer/pacing-{pacing}/{label}"));
-        group.noise_threshold(0.03);
-
         // Benchmark with wallclock time, i.e. measure the compute efficiency.
-        group.bench_function("wallclock-time", |b| {
-            b.iter_batched(
-                setup,
-                |s| {
-                    black_box(s.run());
-                },
-                SmallInput,
-            );
-        });
+        {
+            let mut group = c.benchmark_group(format!("transfer/pacing-{pacing}/{label}"));
+            group.noise_threshold(0.03);
+            group.bench_function("wallclock-time", |b| {
+                b.iter_batched(
+                    setup,
+                    |s| {
+                        black_box(s.run());
+                    },
+                    SmallInput,
+                );
+            });
+        }
 
         // Benchmark with simulated time, i.e. measure the network protocol
         // efficiency.
         //
         // Note: Given that this is using simulated time, we can measure actual
         // throughput.
-        group.throughput(criterion::Throughput::Bytes(TRANSFER_AMOUNT as u64));
-        group.bench_function("simulated-time", |b| {
-            b.iter_custom(|iters| {
-                let mut d_sum = Duration::ZERO;
-                for _i in 0..iters {
-                    // run() returns the simulated time, excluding setup time.
-                    // No need for black_box(), because this doesn't measure compute.
-                    d_sum += setup().run();
-                }
-                d_sum
+        {
+            let mut group = c.benchmark_group(format!("transfer/pacing-{pacing}/{label}"));
+            group.throughput(criterion::Throughput::Bytes(TRANSFER_AMOUNT as u64));
+            group.bench_function("simulated-time", |b| {
+                b.iter_custom(|iters| {
+                    let mut d_sum = Duration::ZERO;
+                    for _i in 0..iters {
+                        // run() returns the simulated time, excluding setup time.
+                        // No need for black_box(), because this doesn't measure compute.
+                        d_sum += setup().run();
+                    }
+                    d_sum
+                });
             });
-        });
-
-        group.finish();
+        }
     }
 }
 
