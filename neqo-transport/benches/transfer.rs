3718163ac928c4e82c238e55017e8806005d3000
Max Leonard Inden <mail@max-inden.de>
2025-09-09 15:48:50 +0200

    Single group

--- neqo-transport/benches/transfer.rs
+++ neqo-transport/benches/transfer.rs
@@ -57,43 +57,35 @@ fn benchmark_transfer(c: &mut Criterion, label: &str, seed: Option<impl AsRef<st
             sim.setup()
         };
 
+        let mut group = c.benchmark_group(format!("transfer/pacing-{pacing}/{label}"));
         // Benchmark with wallclock time, i.e. measure the compute efficiency.
-        {
-            let mut group =
-                c.benchmark_group(format!("transfer/pacing-{pacing}/{label}/wallclock-time"));
-            group.noise_threshold(0.03);
-            group.bench_function("run", |b| {
-                b.iter_batched(
-                    setup,
-                    |s| {
-                        black_box(s.run());
-                    },
-                    SmallInput,
-                );
-            });
-        }
+        group.bench_function("wallclock-time", |b| {
+            b.iter_batched(
+                setup,
+                |s| {
+                    black_box(s.run());
+                },
+                SmallInput,
+            );
+        });
 
         // Benchmark with simulated time, i.e. measure the network protocol
         // efficiency.
         //
         // Note: Given that this is using simulated time, we can measure actual
         // throughput.
-        {
-            let mut group =
-                c.benchmark_group(format!("transfer/pacing-{pacing}/{label}/simulated-time"));
-            group.throughput(criterion::Throughput::Bytes(TRANSFER_AMOUNT as u64));
-            group.bench_function("run", |b| {
-                b.iter_custom(|iters| {
-                    let mut d_sum = Duration::ZERO;
-                    for _i in 0..iters {
-                        // run() returns the simulated time, excluding setup time.
-                        // No need for black_box(), because this doesn't measure compute.
-                        d_sum += setup().run();
-                    }
-                    d_sum
-                });
+        group.throughput(criterion::Throughput::Bytes(TRANSFER_AMOUNT as u64));
+        group.bench_function("simulated-time", |b| {
+            b.iter_custom(|iters| {
+                let mut d_sum = Duration::ZERO;
+                for _i in 0..iters {
+                    // run() returns the simulated time, excluding setup time.
+                    // No need for black_box(), because this doesn't measure compute.
+                    d_sum += setup().run();
+                }
+                d_sum
             });
-        }
+        });
     }
 }
 
