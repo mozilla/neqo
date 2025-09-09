ee0e94e4c46cce8293b3f8e666cea41ba8217ea8
Lars Eggert <lars@eggert.org>
2025-09-09 15:12:17 +0200

    fix: Use `test_fixture::now` in tests
    Instead of `Instant::now`. Might help with #2855.

--- neqo-transport/src/fc.rs
+++ neqo-transport/src/fc.rs
@@ -1044,7 +1044,7 @@ mod test {
     #[test]
     fn trigger_factor() -> Res<()> {
         let rtt = Duration::from_millis(40);
-        let now = Instant::now();
+        let now = test_fixture::now();
         let mut fc = ReceiverFlowControl::new(StreamId::new(0), INITIAL_RECV_WINDOW_SIZE as u64);
 
         let fraction = INITIAL_RECV_WINDOW_SIZE as u64 / WINDOW_UPDATE_FRACTION;
@@ -1065,7 +1065,7 @@ mod test {
     #[test]
     fn auto_tuning_increase_no_decrease() -> Res<()> {
         let rtt = Duration::from_millis(40);
-        let mut now = Instant::now();
+        let mut now = test_fixture::now();
         let mut fc = ReceiverFlowControl::new(StreamId::new(0), INITIAL_RECV_WINDOW_SIZE as u64);
         let initial_max_active = fc.max_active();
 
@@ -1100,7 +1100,7 @@ mod test {
     #[test]
     fn stream_data_blocked_triggers_auto_tuning() -> Res<()> {
         let rtt = Duration::from_millis(40);
-        let now = Instant::now();
+        let now = test_fixture::now();
         let mut fc = ReceiverFlowControl::new(StreamId::new(0), INITIAL_RECV_WINDOW_SIZE as u64);
 
         // Send first window update to give auto-tuning algorithm a baseline.
@@ -1152,7 +1152,7 @@ mod test {
             let rtt = Duration::from_millis(u64::from(random::<1>()[0]) + 1);
             let bdp = bandwidth * u64::try_from(rtt.as_millis()).unwrap() / 1_000 / 8;
 
-            let mut now = Instant::now();
+            let mut now = test_fixture::now();
 
             let mut send_to_recv = VecDeque::new();
             let mut recv_to_send = VecDeque::new();
