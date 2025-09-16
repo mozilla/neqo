67b8bc785140ee8e8b912ee9550ca8a173c6f0db
Lars Eggert <lars@eggert.org>
2025-09-16 12:51:10 +0300

    feat: Don't re-allocate inbound datagram to remove varint
    Fixes #2854
    
    @mxinden, is this what you had in mind with option 1?

--- neqo-http3/src/connection_server.rs
+++ neqo-http3/src/connection_server.rs
@@ -305,7 +305,7 @@ impl Http3ServerHandler {
                         s.stream_writable();
                     }
                 }
-                ConnectionEvent::Datagram(dgram) => self.base_handler.handle_datagram(&dgram),
+                ConnectionEvent::Datagram(dgram) => self.base_handler.handle_datagram(dgram),
                 ConnectionEvent::AuthenticationNeeded
                 | ConnectionEvent::EchFallbackAuthenticationNeeded { .. }
                 | ConnectionEvent::ZeroRttRejected
