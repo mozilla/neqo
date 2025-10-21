c6dccb8ed8269025abf61a9aef92544ce8ad58c0
Lars Eggert <lars@eggert.org>
2025-10-21 17:01:01 +0300

    Update comment

--- test/update-lockfile.sh
+++ test/update-lockfile.sh
@@ -6,11 +6,6 @@ set -euo pipefail
 # installed (while https://github.com/graydon/check-lockfile-intersection has
 # not merged my PR.) Execute at the workspace root, i.e., not inside `test`. It
 # will spit out a list of `cargo update` commands to apply.
-#
-# check-lockfile-intersection doesn't deal with deps that exist in multiple
-# versions, so those are excluded below and will hence need manually updating in
-# `Cargo.lock`. It also doesn't deal with Gecko's ".999" version patching
-# scheme, so some updates will just fail.
 
 if ! command -v check-lockfile-intersection >/dev/null 2>&1; then
     echo "Error: check-lockfile-intersection is not installed or not in PATH." >&2
