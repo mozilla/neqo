d4a53138bfd75d3e505fb74fcb7204a67b162f52
Oskar Mansfeld <git@omansfeld.net>
2025-10-29 13:57:37 +0100

    Adress PR comments:
    - Replace `Vec` with `HashMap` for `maybe_lost_packets` to avoid heap
      allocation in `detect_spurious_congestion_events`
    - Move MaybeLostPacket struct to `classic_cc.rs` and remove from
      `sent.rs`
    - Update `detect_spurious_congestion_event` to increment stats directly
    - Various documentation and code comment changes

--- neqo-transport/src/cc/mod.rs
+++ neqo-transport/src/cc/mod.rs
@@ -78,7 +78,11 @@ pub trait CongestionControl: Display + Debug {
         cc_stats: &mut CongestionControlStats,
     ) -> bool;
 
-    fn detect_spurious_congestion_event(&mut self, acked_packets: &[sent::Packet]) -> bool;
+    fn detect_spurious_congestion_event(
+        &mut self,
+        acked_packets: &[sent::Packet],
+        cc_stats: &mut CongestionControlStats,
+    );
 
     fn cleanup_maybe_lost_packets(&mut self, now: Instant, pto: Duration);
 
