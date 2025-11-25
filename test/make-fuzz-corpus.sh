584819214ec34ae77eba959d3b68184a280981d3
Lars Eggert <lars@eggert.org>
2025-11-25 14:07:19 +0200

    Updates

--- test/make-fuzz-corpus.sh
+++ test/make-fuzz-corpus.sh
@@ -2,37 +2,33 @@
 
 set -euo pipefail
 
-# If we were passed a directory, use that as a temporary location to store
-# generated fuzzing corpus files. Otherwise, create a temporary directory
-# that will be deleted on exit.
-if [ $# -eq 1 ] && [ -d "$1" ]; then
-    NEQO_CORPUS="$1"
-else
-    NEQO_CORPUS=$(mktemp -d)
-    trap 'rm -rf "$NEQO_CORPUS"' EXIT
-fi
+TMP=$(mktemp -d)
+trap 'rm -rf "$TMP"' EXIT
 
 # First, let's run the test suite to generate initial fuzzing corpora.
 # This will have failing tests, ignore them.
-echo "Generating new fuzzing corpora under $NEQO_CORPUS..."
-cargo test --quiet --locked --features build-fuzzing-corpus --no-fail-fast > /dev/null 2>&1 || true
+echo "Generating new fuzzing corpora under $TMP..."
+NEQO_CORPUS=$TMP cargo test --quiet --locked --features build-fuzzing-corpus --no-fail-fast > /dev/null 2>&1 || true
+
+# Do dev builds, since we just merge corpora and don't actually run fuzzers here.
+# LTO-linking otherwise takes longer than the merging.
+echo "Building all fuzzers..."
+cargo fuzz build --dev > /dev/null 2>&1
 
 # Now, only merge in those newly-generated samples that increase coverage.
 # "cargo fuzz" cannot do this, so use the underlying LLVM fuzzer binary directly.
 TRIPLE="$(basename "$(dirname "$(rustc --print target-libdir)")")"
 for fuzzer in $(cargo fuzz list); do
     echo
-    generated="$NEQO_CORPUS/$fuzzer"
+    generated="$TMP/$fuzzer"
     if [ ! -d "$generated" ]; then
         echo "$fuzzer fuzzer: WARNING, test suite generated no corpus"
         continue
     fi
-    echo "$fuzzer fuzzer: Building..."
-    cargo fuzz build "$fuzzer" > /dev/null 2>&1
     corpus="fuzz/corpus/$fuzzer"
     before=$(find "$corpus" -type f | wc -l | tr -d ' ')
     echo "$fuzzer fuzzer: Merging new unique samples into corpus ($before samples before)..."
-    "target/$TRIPLE/release/$fuzzer" -merge=1 "$corpus" "$generated" 2> /dev/null
+    "target/$TRIPLE/debug/$fuzzer" -merge=1 "$corpus" "$generated" 2> /dev/null
     after=$(find "$corpus" -type f | wc -l | tr -d ' ')
     diff=$((after - before))
     echo "$fuzzer fuzzer: $diff new samples added (now $after)"
