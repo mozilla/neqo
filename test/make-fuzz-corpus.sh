08ab46db1e8798452d5a633cbdb223eaa13b7251
Lars Eggert <lars@eggert.org>
2025-11-25 08:52:22 +0200

    Make PR with new fuzzing samples

--- test/make-fuzz-corpus.sh
+++ test/make-fuzz-corpus.sh
@@ -2,19 +2,27 @@
 
 set -euo pipefail
 
+# If we were passed a directory, use that as a temporary location to store
+# generated fuzzing corpus files. Otherwise, create a temporary directory
+# that will be deleted on exit.
+if [ $# -eq 1 ] && [ -d "$1" ]; then
+    NEQO_CORPUS="$1"
+else
+    NEQO_CORPUS=$(mktemp -d)
+    trap 'rm -rf "$NEQO_CORPUS"' EXIT
+fi
+
 # First, let's run the test suite to generate initial fuzzing corpora.
 # This will have failing tests, ignore them.
-TMP=$(mktemp -d)
-trap 'rm -rf "$TMP"' EXIT
-echo "Generating new fuzzing corpora under $TMP..."
-NEQO_CORPUS=$TMP cargo test --quiet --locked --features build-fuzzing-corpus --no-fail-fast > /dev/null 2>&1 || true
+echo "Generating new fuzzing corpora under $NEQO_CORPUS..."
+cargo test --quiet --locked --features build-fuzzing-corpus --no-fail-fast > /dev/null 2>&1 || true
 
 # Now, only merge in those newly-generated samples that increase coverage.
 # "cargo fuzz" cannot do this, so use the underlying LLVM fuzzer binary directly.
 TRIPLE="$(basename "$(dirname "$(rustc --print target-libdir)")")"
 for fuzzer in $(cargo fuzz list); do
     echo
-    generated="$TMP/$fuzzer"
+    generated="$NEQO_CORPUS/$fuzzer"
     if [ ! -d "$generated" ]; then
         echo "$fuzzer fuzzer: WARNING, test suite generated no corpus"
         continue
