6673e07eb380d5ea66d87339b310decc7f79ce08
Lars Eggert <lars@eggert.org>
2025-11-27 11:30:49 +0200

    Don't hide errors

--- test/make-fuzz-corpus.sh
+++ test/make-fuzz-corpus.sh
@@ -8,12 +8,12 @@ trap 'rm -rf "$TMP"' EXIT
 # First, let's run the test suite to generate initial fuzzing corpora.
 # This will have failing tests, ignore them.
 echo "Generating new fuzzing corpora under $TMP..."
-NEQO_CORPUS=$TMP cargo test --quiet --locked --features build-fuzzing-corpus --no-fail-fast > /dev/null 2>&1 || true
+NEQO_CORPUS=$TMP cargo test --quiet --locked --features build-fuzzing-corpus --no-fail-fast || true
 
 # Do dev builds, since we just merge corpora and don't actually run fuzzers here.
 # LTO-linking otherwise takes longer than the merging.
 echo "Building all fuzzers..."
-cargo fuzz build --dev > /dev/null 2>&1
+cargo fuzz build --dev
 
 # Now, only merge in those newly-generated samples that increase coverage.
 # "cargo fuzz" cannot do this, so use the underlying LLVM fuzzer binary directly.
@@ -27,7 +27,7 @@ for fuzzer in $(cargo fuzz list); do
     corpus="fuzz/corpus/$fuzzer"
     before=$(find "$corpus" -type f | wc -l | tr -d ' ')
     echo "$fuzzer fuzzer: Merging new unique samples into corpus ($before samples currently)..."
-    "target/$TRIPLE/debug/$fuzzer" -merge=1 "$corpus" "$generated" 2> /dev/null
+    "target/$TRIPLE/debug/$fuzzer" -merge=1 "$corpus" "$generated"
     after=$(find "$corpus" -type f | wc -l | tr -d ' ')
     diff=$((after - before))
     echo "$fuzzer fuzzer: $diff new samples added (now $after)"
