feab4daebc9ebcebef045bd8a806f3afcea79691
Lars Eggert <lars@eggert.org>
2026-01-20 19:45:11 +0200

    Again

--- mtu/src/bindings/linux.h
+++ mtu/src/bindings/linux.h
@@ -1,1 +1,7 @@
 #include <linux/rtnetlink.h>
+
+/* Force bindgen to generate these types */
+struct nlmsghdr __nlmsghdr;
+struct rtattr __rtattr;
+struct rtmsg __rtmsg;
+struct ifinfomsg __ifinfomsg;
