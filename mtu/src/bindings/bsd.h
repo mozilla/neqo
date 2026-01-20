774e5d99b3917ffce240c5e0b2b108fb6a0064b0
Lars Eggert <lars@eggert.org>
2026-01-20 19:45:11 +0200

    Again

--- mtu/src/bindings/bsd.h
+++ mtu/src/bindings/bsd.h
@@ -2,3 +2,8 @@
 #include <sys/socket.h>
 #include <net/route.h>
 #include <net/if.h>
+
+/* Force bindgen to generate these types */
+struct rt_msghdr __rt_msghdr;
+struct rt_metrics __rt_metrics;
+struct if_data __if_data;
