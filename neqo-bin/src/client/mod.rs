9ba9280918936ff35196f1034a59fdecdc5714e4
Lars Eggert <lars@eggert.org>
2026-05-11 09:07:25 +0300

    chore: Benchmark "bypass SSL AEAD wrapper in RecordProtection"
    https://github.com/mozilla/nss-rs/pull/49

--- neqo-bin/src/client/mod.rs
+++ neqo-bin/src/client/mod.rs
@@ -644,9 +644,7 @@ pub async fn client(mut args: Args) -> Res<()> {
 
                 let handler = http3::Handler::new(to_request, args.clone());
 
-                Runner::new(real_local, &mut socket, client, handler, &args)
-                    .run()
-                    .await?
+                Box::pin(Runner::new(real_local, &mut socket, client, handler, &args).run()).await?
             } else {
                 let client = http09::create_client(&args, real_local, remote_addr, &host, token)
                     .expect("failed to create client");
